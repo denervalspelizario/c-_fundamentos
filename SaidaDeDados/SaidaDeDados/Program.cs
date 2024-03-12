dad892a4f3747783a34ee7a61ae9e440f09711f9
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-03-12 09:30:46 -0300

    estudo c# uso de concatenacao e interpolacao de strings

---
+++ SaidaDeDados/SaidaDeDados/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Saida de Dados");
+Console.WriteLine();
+
+
+int idade = 25;
+string nome = "Maria";
+
+// Uso de interpolaçao e concatenaçãod e strings no c# 
+
+Console.WriteLine(nome + " tem " + idade + " anos de idade."); // concatenação
+Console.WriteLine($"{nome} tem {idade} anos de idade;"); // interpolação
+
+
+Console.ReadLine();
