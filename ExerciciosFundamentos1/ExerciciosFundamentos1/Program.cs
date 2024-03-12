e4431f2587e29f25de4238380aca2869fdfa65fa
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-03-12 09:09:41 -0300

    estudo c#  exercicios fundamentos c# variaveis , constantes e nullables types

---
+++ ExerciciosFundamentos1/ExerciciosFundamentos1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Fase 01");
+Console.WriteLine();
+
+
+int idade = 35;  // dado inteiro 
+string nome = "Maria";  // variavel tipo string
+double altura = 3.45; // variavel tipo float
+DateTime data = new DateTime(1999,09,04);  // classe datetime para manipular data e hora
+const int ano = 12;  // constante int
+double? nota = 7.80; // variable tipo double que aceita null(nullables types)
+int x = 66;
+int y = 77;
+int soma = x + y;
+
+
+
+
+
+
+
+
+Console.WriteLine(idade);
+Console.WriteLine(altura);
+Console.WriteLine(nome);
+Console.WriteLine(data.ToShortDateString());
+Console.WriteLine(ano);
+Console.WriteLine(nota);
+Console.WriteLine(soma);
