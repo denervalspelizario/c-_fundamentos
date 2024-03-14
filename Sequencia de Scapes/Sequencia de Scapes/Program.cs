37d62b1fdc54d7b0c0d3a316a99726c83c1cecea
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-03-14 15:04:49 -0300

    estudo c# sequencia de scapes e conversao de tipos de dados

---
+++ Sequencia de Scapes/Sequencia de Scapes/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Sequencia_de_Scapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Como usar \ ou "" dentro de uma string?
+            // Usando sequencia de scapes  neste caso será
+
+
+            Console.WriteLine("Saida de dados : Usando sequencia de scapes");
+
+
+            // no caso de uso de \ dentro de uma string
+            string local = "c: \\dados\\poesias.txt"; // seria como "c: \dados\poesias.txt"
+
+
+            // no caso de uso de "" aspas duplas
+            string frase = "Ele falou: \"Não fui eu \"";  // seria como "Ele falou: "Não fui eu" "
+
+
+
+
+        }
+    }
+}
