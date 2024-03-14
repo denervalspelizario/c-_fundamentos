06a16696cf6bca9195e04a9e0743e69133581923
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-03-14 15:48:35 -0300

    estudo c# uso de readline para entrada de dados , operadores aritimeticos básicos

---
+++ EntradaDeDados/EntradaDeDados/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Channels;
+
+Console.WriteLine("Entrada de Dados");
+
+Console.WriteLine("\nInforme o seu nome completo");
+
+string nomeCompleto = Console.ReadLine(); // recebendo o dado digitado
+
+Console.WriteLine("\nInforme a sua idade");
+
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nInfome o dia do seu nascimento");
+int diaNascimento = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("\nO seu nome é " + nomeCompleto); // imprimindo atravez de uma concatenação 
+
+Console.WriteLine($"\nA sua idade é {idade}");
+
+Console.WriteLine("\nO dia do seu nascimento foi " + diaNascimento);
+
+Console.ReadKey();
---
+++ OperadoresAritimeticos/OperadoresAritimeticos/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Operadores Aritméticos");
+
+Console.WriteLine("Informe o valor de x");
+
+int x = Convert.ToInt32(Console.ReadLine());  // basicamente eu to pegando pegando o dado digitado no console
+                                              // que vem como string e convertendo para int 
+
+Console.WriteLine("Informe o valor de y");
+
+int y = Convert.ToInt32(Console.ReadLine());  // basicamente eu to pegando pegando o dado digitado no console
+                                              // que vem como string e convertendo para int 
+
+Console.WriteLine($"soma de x + y = {x + y}");
+Console.WriteLine($"subtração de x - y = {x - y}");
+double divisao = x / y;
+Console.WriteLine($"divisão de x / y = {divisao}");
+Console.WriteLine($"multiplicação de x * y = {x * y}");
+
+
+// Uso de algumas operãções usando a classe Math
+
+Console.WriteLine($"\nRaiz quadrada de x {Math.Sqrt(x)}");
+Console.WriteLine($"\nPotencia de x elevado a y {Math.Pow(x,y)}");
+Console.WriteLine($"\nValor mínimo entre x e y {Math.Min(x,y)}");
+Console.WriteLine($"\nValor maximo entre x e y {Math.Max(x, y)}");
+Console.WriteLine($"\nCoseno de x  {Math.Cos(x)}");
+
+
+
+
+Console.ReadKey();
