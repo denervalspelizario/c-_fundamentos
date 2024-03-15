2c05697cc68f2bf5d9b2438d4f70bc1fdb2c1c29
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-03-15 11:36:37 -0300

    estudo c# tipo var e operadores de atribuição

---
+++ InferenciasTipoVar/InferenciasTipoVar/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("## Inferência Tipos (var)");
+Console.WriteLine();
+
+// o uso de var no c# é possivel vc está deixando na mão da linguagem a tipificação da variável
+var idade = 25;
+var nome = "Maria";
+var salario = 2500.00m;
+
+
+Console.WriteLine($"{nome} tem {idade} anos e ganha {salario}");
+
+
+var teste = new Teste();  // estou adicionando a varaivel teste uma nova classe Teste que chama o metodoMeuMetodo
+
+teste.MeuMetodo(); // como esta em (); então ele inicializa sozinho 
+
+
+
+
+// classe teste que sera ativada
+class Teste
+{
+    public void MeuMetodo()
+    {
+        Console.WriteLine("Sistema de Teste inicializado");
+    }
+}
---
+++ OperadoresDeAtribuicao/OperadoresDeAtribuicao/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Operadores de Atribuição");
+
+
+// usando operadores de atribuição com tipos numéricos
+
+var x = 10;
+
+
+Console.WriteLine($"O Valor inicial de x = {x}"); // 10
+Console.WriteLine($"x += 5 o resultado será  {x += 5}"); // 15
+Console.WriteLine($"x -= 5 o resultado será  {x -= 5}"); // 12
+Console.WriteLine($"x *= 5 o resultado será  {x *= 5}"); // 48
+Console.WriteLine($"x /= 5 o resultado será  {x /= 5}"); // 9
+Console.WriteLine($"x %= 5 o resultado será  {x %= 5}"); // 4
+
+// da para fazer atribuição com + com strings segue exemplo
+
+var dado = "123";
+
+dado += "456";
+
+Console.WriteLine(dado); // o resultado de da variavel dado será 123456
+
+Console.ReadLine();
